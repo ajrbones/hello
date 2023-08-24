96cbb42eb0dc4cc79703a223472464ccb685e714
Alex Robinson <arobinson44@huskers.unl.edu>
2023-08-24 08:52:07 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Alexander Robinson
+ *         arobinson44@huskers.unl.edu
+ * Date: 2023/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Alex Robinson\n");
+  return 0;
+}
